76a8a4c8fd3879cc6f8c378058889be28e932022
Hinaser <rindo.hinase@gmail.com>
2018-09-07 06:44:26 +0000

    Initial commit for gh-pages

--- src/App.js
+++ src/App.js
@@ -1,125 +1,18 @@
 import React, { Component } from 'react';
-import { Slide, Fade, Zoom } from './lib';
-import './app.css';
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
   render() {
-    const slideImages = [
-      'images/slide_2.jpg',
-      'images/slide_3.jpg',
-      'images/slide_4.jpg'
-    ];
-
-    const fadeImages = [
-      'images/slide_5.jpg',
-      'images/slide_6.jpg',
-      'images/slide_7.jpg'
-    ];
-
-    const zoomOutImages = [
-      'images/slide_2.jpg',
-      'images/slide_7.jpg',
-      'images/slide_5.jpg'
-    ];
-
-    const properties = {
-      duration: 5000,
-      transitionDuration: 500,
-      infinite: true,
-      indicators: true
-    }
-
-    const fadeProperties = {
-      duration: 5000,
-      transitionDuration: 500,
-      infinite: true,
-      indicators: true,
-      direction: "in"
-    }
-
-    const zoomOutProperties = {
-      duration: 5000,
-      transitionDuration: 500,
-      infinite: true,
-      indicators: true,
-      scale: 0.4
-    }
-
-    const zoomInProperties = {
-      duration: 5000,
-      transitionDuration: 500,
-      infinite: true,
-      indicators: true,
-      scale: 1.4
-    }
-
     return (
-      <div>
-        <h3>Slide Effect</h3>
-        <div className="slide-container">
-          <Slide {...properties}>
-            <div className="each-slide">
-              <div style={{'backgroundImage': `url(${slideImages[0]})`}}>
-                <span>Slide 1</span>
-              </div>
-            </div>
-            <div className="each-slide">
-              <div style={{'backgroundImage': `url(${slideImages[1]})`}}>
-                <span>Slide 2</span>
-              </div>
-            </div>
-            <div className="each-slide">
-              <div style={{'backgroundImage': `url(${slideImages[2]})`}}>
-                <span>Slide 3</span>
-              </div>
-            </div>
-          </Slide>
-        </div>
-        <br />
-        <h3>Fade Effect</h3>
-        <div className="slide-container">
-          <Fade {...fadeProperties}>
-            <div className="each-fade">
-              <div className="image-container">
-                <img src={fadeImages[0]} />
-              </div>
-              <h2>First Slide</h2>
-            </div>
-            <div className="each-fade">
-              <div className="image-container">
-                <img src={fadeImages[1]} />
-              </div>
-              <h2>Second Slide</h2>
-            </div>
-            <div className="each-fade">
-              <div className="image-container">
-                <img src={fadeImages[2]} />
-              </div>
-              <h2>Third Slide</h2>
-            </div>
-          </Fade>
-        </div>
-
-        <br />
-        <h3>Zoom out Effect</h3>
-        <div className="slide-container">
-          <Zoom {...zoomOutProperties}>
-            {
-              zoomOutImages.map((each, index) =><img key={index} style={{width: "100%"}} src={each} />)
-            }
-          </Zoom>
-        </div>
-
-        <br />
-        <h3>Zoom in Effect</h3>
-        <div className="slide-container">
-          <Zoom {...zoomInProperties}>
-            {
-              fadeImages.map((each, index) =><img key={index} style={{width: "100%"}} src={each} />)
-            }
-          </Zoom>
-        </div>
-        <br />
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
       </div>
     );
   }
